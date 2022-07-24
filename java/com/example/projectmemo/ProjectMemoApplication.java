672892663cf48804d62bd4ca48bcebb609b5075e
jack <jachky1205@gmail.com>
2022-07-24 17:09:01 +0900

    reinforce GET for addHTML, connect timestamped

--- java/com/example/projectmemo/ProjectMemoApplication.java
+++ java/com/example/projectmemo/ProjectMemoApplication.java
@@ -2,7 +2,9 @@ package com.example.projectmemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA사용 시 데이터 변동이 있으면 자동으로 반영
 @SpringBootApplication
 public class ProjectMemoApplication {
 
--- java/com/example/projectmemo/domain/Timestamped.java
+++ java/com/example/projectmemo/domain/Timestamped.java
@@ -2,6 +2,7 @@
 
 package com.example.projectmemo.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter //빠뜨리지 않도록 주의
 @MappedSuperclass // TimeStamped를 상속하여 Entity가 자동으로 데이터베이스 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
 //abstract 상속으로만 메소드를 만드는 것
